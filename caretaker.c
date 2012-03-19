1d48dca302e5ca3ca966ae234428954a73680161
Hexcles Ma <bob1211@gmail.com>
2012-03-20 07:54:58 +0800

    Change to FreeBSD License

--- caretaker.c
+++ caretaker.c
@@ -1,22 +1,33 @@
-/* 
+/*
  * caretaker.c
  *  
  * This file is part of Eevee.
  * 
- * Copyright (C) 2012 Hexcles Ma <bob1211@gmail.com>
+ * Copyright (C) 2012, Hexcles Ma <bob1211@gmail.com>
+ * All rights reserved.
  * 
- * Eevee is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
  * 
- * Eevee is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following disclaimer
+ *   in the documentation and/or other materials provided with the
+ *   distribution.
  * 
- * You should have received a copy of the GNU General Public License
- * along with Eevee.  If not, see <http://www.gnu.org/licenses/>.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /*
@@ -88,14 +99,13 @@ inline int tv2ms(struct timeval tv) {
 }
 
 char *path_cat(const char *path, char *file) {
-	size_t path_len = strlen(path);
-	size_t file_len = strlen(file);
-	char *result, *p;
-	p = result = malloc((path_len + file_len + 2) * sizeof(*result));
-	strcpy(p, path); p += path_len;
-	*(p++) = '/';
-	strcpy(p, file); p += file_len;
-	*p = '\0';
+	size_t path_len = strlen(path), file_len = strlen(file);
+	char *result;
+	result = malloc((path_len + file_len + 2) * sizeof(*path));
+	strcpy(result, path);
+	result[path_len] = '/';
+	strcpy(result + path_len + 1, file);
+	//result[path_len + 1 + file_len] = '\0';
 	return result;
 }
 
